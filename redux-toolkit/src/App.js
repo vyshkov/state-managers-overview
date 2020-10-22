bc89e1b0bb55c7a221d1292e462f05437a362599
vvyshko <vvyshko@talend.com>
2020-10-22 13:16:14 +0300

    Added animation

--- redux-toolkit/src/App.js
+++ redux-toolkit/src/App.js
@@ -1,22 +1,25 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-import 'rc-checkbox/assets/index.css';
-import Checkbox from 'rc-checkbox';
+import "rc-checkbox/assets/index.css";
+import Checkbox from "rc-checkbox";
 
-import {TransitionGroup, CSSTransition} from 'react-transition-group'; // ES6
-
+import { TransitionGroup, CSSTransition } from "react-transition-group"; // ES6
 
 const ListToolbar = () => (
   <div className="note-list-toolbar">
-    <input className="note-input" placeholder="Please type your note..."></input>
+    <input
+      className="note-input"
+      placeholder="Please type your note..."
+    ></input>
   </div>
 );
 
 const Note = ({ text, bg, checked, onToggle, onSelect }) => (
-  <button 
-    className="note-item" style={{ backgroundColor: bg }}
+  <button
+    className="note-item"
+    style={{ backgroundColor: bg }}
     onClick={() => {
       if (onSelect) {
         onSelect();
@@ -24,56 +27,67 @@ const Note = ({ text, bg, checked, onToggle, onSelect }) => (
     }}
   >
     {text}
-    <div className={`checkbox-container ${checked ? 'checked' : ''}`}>
-      <Checkbox checked={checked} onClick={e => {
-        e.stopPropagation();
-        if (onToggle) {
-          onToggle(!checked);
-        }
-      }} />
+    <div className={`checkbox-container ${checked ? "checked" : ""}`}>
+      <Checkbox
+        checked={checked}
+        onClick={(e) => {
+          e.stopPropagation();
+          if (onToggle) {
+            onToggle(!checked);
+          }
+        }}
+      />
     </div>
   </button>
-)
+);
 
-const SelectedNote = ({ text, color="#f2f3c3", onTextChange, onColorChange, onClose, inasd }) => (
-  <CSSTransition in={inasd} key={text} className="selected-note-drawer" classNames="fade" timeout={1000} unmountOnExit>
-      <div className="">
-        <textarea
-          value={text}
-          onChange={e => {
-            if (onTextChange) {
-              onTextChange(e.target.value)
-            }
-          }}
-          className="selected-note-text">
-            {text}
-        </textarea>
-        <input
-          className="selected-note-color-picker" type="color"
-          value={color}
-          onChange={e => {
-            if (onColorChange) {
-              onColorChange(e.target.value)
-            }
-          }}
-        />
-        <button onClick={onClose}>Close</button>
-      </div>
-  </CSSTransition>
-  
-)
-
+const SelectedNote = ({
+  text,
+  color = "#f2f3c3",
+  onTextChange,
+  onColorChange,
+  onClose,
+  inasd,
+}) => (
+  <div className="selected-note-drawer">
+    <textarea
+      value={text}
+      onChange={(e) => {
+        if (onTextChange) {
+          onTextChange(e.target.value);
+        }
+      }}
+      className="selected-note-text"
+    >
+      {text}
+    </textarea>
+    <input
+      className="selected-note-color-picker"
+      type="color"
+      value={color}
+      onChange={(e) => {
+        if (onColorChange) {
+          onColorChange(e.target.value);
+        }
+      }}
+    />
+    <button onClick={onClose}>Close</button>
+  </div>
+);
 
 function App() {
-  const [selected, setSelected] = useState([]);
-  console.log('>>>', selected)
+  const [selected, setSelected] = useState(false);
 
   return (
     <div className="App">
       <div className="notes-list">
         <ListToolbar />
-        <div className="nodes-list-items" >
-          <Note text="Hello world" onSelect={() =>setSelected(['hey'])} onToggle={() => alert(2)}/>
+        <div className="nodes-list-items">
+          <Note
+            text="Hello world"
+            onSelect={() => setSelected(true)}
+            onToggle={() => alert(2)}
+          />
           <Note text="Hello world" />
           <Note text="Hello world" checked />
           <Note text="Hello world" />
@@ -89,26 +103,26 @@ function App() {
           <Note text="Hello world" />
           <Note text="Hello world" />
           <Note text="Hello world" />
-          <Note text="Hello world" bg="#dfcec2"/>
-          <Note text="Hello world" bg="#8855AA"/>
+          <Note text="Hello world" bg="#dfcec2" />
+          <Note text="Hello world" bg="#8855AA" />
         </div>
-       
       </div>
       <div className="list-stats">Total number of items: 0</div>
-      
-      <TransitionGroup className="selected">
-           { selected.map(() => (
-              <SelectedNote 
-                  text="123"
-                  onTextChange={(val) => console.log('text', val)}
-                  onColorChange={(color) => { console.log('color', color) }}
-                  onClose={() =>  setSelected([])}
-                  inasd={!!selected}
-              />
-            ))}
-     </TransitionGroup>
 
-     
+      <TransitionGroup className="drawer-transition">
+        {selected && (
+          <CSSTransition className="PAR" classNames="fade" timeout={100000}>
+            <SelectedNote
+              text="123"
+              onTextChange={(val) => console.log("text", val)}
+              onColorChange={(color) => {
+                console.log("color", color);
+              }}
+              onClose={() => setSelected(false)}
+            />
+          </CSSTransition>
+        )}
+      </TransitionGroup>
     </div>
   );
 }
